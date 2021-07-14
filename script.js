3646bfc2ccbecea8b9d1ee145630c1ec7720b214
Jaimes77 <JaimeRGoff@gmail.com>
2021-07-14 15:55:09 -0400

    final result

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 const baseURL =
-  "https://api.nasa.gov/mars-photos/api/v1/rovers/manifests/Curiosity";
+  "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos";
 const key = "tDl9btMFVvJuRAfDTJVPaOZHeqHlHQ7ta4uomTWj";
 
 const row = document.querySelector(".row");
 
-fetch(`${baseURL}?api_key=${key}`)
+fetch(`${baseURL}?earth_date=2012-08-06&page=1&api_key=${key}`)
   .then((response) => response.json())
   .then((json) => displayResults(json));
 
 function displayResults(json) {
   console.log(json.photos);
 
-  // json.photos.forEach(function (photo) {
-  //   console.log(photo);
+  json.photos.forEach(function (photo) {
+    console.log(photo);
 
-  //   let card = document.createElement("div");
-  //   card.className = "card text-white bg-dark mb-3";
-  //   card.style =
-  //     "width: 18rem; margin: 1em; height: 400px; padding: .5em; overflow: auto";
+    let column = document.createElement("div");
+    column.className = "col";
 
-  //   let img = document.createElement("img");
-  //   img.className = "card-img-top";
-  //   img.src = photo.img_src;
+    let card = document.createElement("div");
+    card.className = "card text-white border-light bg-dark mb-3";
+    card.style = "width: 15rem; margin: 1em; height: 400px; padding: .2em; ";
 
-  //   let cardBody = document.createElement("div");
-  //   cardBody.className = "card-body";
+    let img = document.createElement("img");
+    img.className = "card-img-top rounded";
+    img.src = photo.img_src;
 
-  //   let cardText = document.createElement("p");
-  //   cardText.className = "card-text";
-  //   cardText.innerText = photo.earth_date;
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
 
-  //   row.appendChild(card);
-  //   card.appendChild(img);
-  //   card.appendChild(cardBody);
-  //   cardBody.appendChild(cardText);
-  // });
+    let cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.innerText = photo.earth_date + ": " + photo.camera.full_name;
+
+    row.appendChild(column);
+    column.appendChild(card);
+    card.appendChild(img);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardText);
+  });
 }
